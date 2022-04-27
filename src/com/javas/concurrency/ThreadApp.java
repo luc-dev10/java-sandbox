ffcf4c6a35eecb21508879a348bdf4528b171479
luc-dev10 <luc.dev1008@gmail.com>
2022-04-26 22:24:59 -0400

    adding anonymous class for thread

--- src/com/javas/concurrency/ThreadApp.java
+++ src/com/javas/concurrency/ThreadApp.java
@@ -6,11 +6,21 @@ public class ThreadApp {
      *  every process has its own head memory
      * */
     public static void main(String[] args) {
+
         System.out.println("Main Thread.....");
 
         // next thread
         FirstThread firstThread = new FirstThread();
         firstThread.start();
+
+        // creating anonymous class
+        new Thread() {
+            public void run() {
+                System.out.println("Anonymous Thread...");
+            }
+        }.start();
+
+        System.out.println("Main Thread ending.....");
 
     }
 }

8d2ce484fc653bff318d571e92729b3721b158a6
luc-dev10 <luc.dev1008@gmail.com>
2022-04-27 22:00:28 -0400

    joining threads

--- src/com/sandbox/concurrency/ThreadApp.java
+++ src/com/sandbox/concurrency/ThreadApp.java
@@ -25,7 +25,19 @@ public class ThreadApp {
         // ________________________________
 
         // creating threads with the Runnable Interface
-        Thread runnableThread = new Thread(new FirstRunnable());
+        Thread runnableThread = new Thread(new FirstRunnable() {
+            @Override
+            public void run() {
+                System.out.println("First Runnable Running with runny legs...");
+                try {
+                    firstThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("First Runnable waited for first thread to complete...");
+            }
+        });
         runnableThread.start();
 
         // anonymous thread and runnable
@@ -39,7 +51,11 @@ public class ThreadApp {
         // ________________________________
 
         // thread interruption
-        firstThread.interrupt();
+        // firstThread.interrupt();
+
+        // ________________________________
+
+        // waiting for thread to finalize execution
 
         System.out.println("Main Thread ending.....");
     }
---
+++ src/com/sandbox/lambda/MainApp.java
@@ -0,0 +1,7 @@
+package com.sandbox.lambda;
+
+public class MainApp {
+    public static void main(String[] args) {
+        
+    }
+}

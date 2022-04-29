17e817fafca40329fcafa4e4fa7a7d6db66518b9
luc-dev10 <luc.dev1008@gmail.com>
2022-04-28 21:30:06 -0400

    lambda expressions and functional interfaces

--- src/com/sandbox/lambda/MainApp.java
+++ src/com/sandbox/lambda/MainApp.java
@@ -2,6 +2,10 @@ package com.sandbox.lambda;
 
 public class MainApp {
     public static void main(String[] args) {
-        
+
+        // using lambda expression for thread
+        Thread thread = new Thread(() -> System.out.println("Thread is running"));
+        thread.start();
+
     }
 }

dfde89c1c80bcd096c21bcf84f67a28c6c34548b
luc-dev10 <luc.dev1008@gmail.com>
2022-04-30 15:08:27 -0400

    function interface

--- src/com/sandbox/lambda/Chair.java
+++ src/com/sandbox/lambda/Chair.java
@@ -1,6 +1,26 @@
 package com.sandbox.lambda;
 
 public class Chair {
+
+    private int id;
+    private char name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public char getName() {
+        return name;
+    }
+
+    public void setName(char name) {
+        this.name = name;
+    }
+
     private int leg;
 
     public Chair(int leg) {
--- src/com/sandbox/lambda/FunctionalPredicate.java
+++ src/com/sandbox/lambda/FunctionalPredicate.java
@@ -1,28 +1,39 @@
 package com.sandbox.lambda;
 
 import java.util.ArrayList;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class FunctionalPredicate {
     public static void main(String[] args) {
 
         ArrayList<Chair> chairs = new ArrayList<>();
-        for (int i = 0; i < 10; i++)
-            chairs.add(new Chair(i));
-
+        for (int i = 0; i < 10; i++) {
+            Chair chair = new Chair(i);
+            chair.setId(i);
+            chair.setName((char) ('A' + i));
+            chairs.add(chair);
+        }
         // using test on foreach
         Predicate<Chair> chairOver10Legs = chair -> chair.getLeg() >= 4;
         for (Chair chair : chairs)
             if (chairOver10Legs.test(chair))
                 System.out.println(chair.getLeg());
 
-        // chaining predicates -
+        // chaining predicates
         Predicate<Chair> chairOver4Legs = chair -> chair.getLeg() % 4 == 0;
         Predicate<Chair> chairOver2Legs = chair -> chair.getLeg() % 2 == 0;
         for (Chair chair : chairs)
             if (chairOver4Legs.and(chairOver2Legs)
                               .test(chair))
                 System.out.println(chair.getLeg());
-        
+
+        // function
+        Function<Chair, String> getDescription = (Chair chair) -> {
+            return String.format("ID: %d | Name: %c", chair.getId(), chair.getName());
+        };
+        for (Chair chair : chairs)
+            System.out.println(getDescription.apply(chair));
+
     }
 }

cbcbc54491c926d8e95ed65aa9c6026d20d76d84
luc-dev10 <luc.dev1008@gmail.com>
2022-05-01 22:10:57 -0400

    non stream implementation

---
+++ src/com/sandbox/stream/MainApp.java
@@ -0,0 +1,25 @@
+package com.sandbox.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        List<Person> personList = new ArrayList<Person>(List.of(
+                new Person("John", 23),
+                new Person("Cena", 32),
+                new Person("Jane", 20),
+                new Person("Doe", 27)
+        ));
+
+        // sort by name
+        personList.sort((s1, s2) -> s1.getName()
+                                      .compareTo(s2.getName()));
+
+        // display
+        personList.forEach(person -> System.out.println(person.getName()));
+
+        // ____________________________________
+    }
+}
